c981ee4be2e061925390a19f3c74e5fc953394e7
Peter-waithaka <peterwaithaka120@gmail.com>
2020-07-27 03:52:53 +0300

    correctected toggle

--- js/index.js
+++ js/index.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
-      $(".design").click(function(){
-        $(".one,#design").toggle("slow");
-      });
-      
+  $(".design").click(function() {
+    $("#design, .one").toggle("slow");
+  });
+  $("#design").click(function() {
+    $(".one, #design").toggle("slow");
+  });
+  $(".development").click(function() {
+    $("#development, .three").toggle("slow");
+  });
+  $("#development").click(function() {
+    $(".three, #development").toggle("slow");
+  });
+  $(".product").click(function() {
+    $("#product, .two").toggle("slow");
+  });
+  $("#product").click(function() {
+    $(".two, #product").toggle("slow");
+  });
 })
